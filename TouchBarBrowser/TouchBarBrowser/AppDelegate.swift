e9e4943cdfd80c828a57f6c1bd838dfcbff50f5f
佐藤秀輔 <sato.fun0619@gmail.com>
2017-04-22 00:14:23 +0900

    [add]プロジェクトファイルの追加

---
+++ TouchBarBrowser/TouchBarBrowser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBarBrowser
+//
+//  Created by 佐藤秀輔 on 2017/04/22.
+//  Copyright © 2017年 佐藤秀輔. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBarBrowser/TouchBarBrowser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarBrowser
+//
+//  Created by 佐藤秀輔 on 2017/04/22.
+//  Copyright © 2017年 佐藤秀輔. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
